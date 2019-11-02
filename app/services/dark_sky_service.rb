13e1188a3ab1e8ce94f9c2be2932b4b4a76d2a84
nancylee713 <srlee713@gmail.com>
2019-11-02 13:52:45 -0600

    Add DarkSkyService class

---
+++ app/services/dark_sky_service.rb
@@ -0,0 +1,24 @@
+class DarkSkyService
+  def self.get_data(location)
+    new.get_data(location)
+  end
+
+  def get_data(location)
+    lat = location.latitude
+    lng = location.longitude
+
+    response = conn.get "#{lat},#{lng}", {
+      exclude: 'flags'
+    }
+
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    key = ENV['dark_sky_api']
+    Faraday.new(:url => "https://api.darksky.net/forecast/#{key}/") do |faraday|
+      faraday.request :url_encoded
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+end
