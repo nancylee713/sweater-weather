99b82c7c649fb1eee5edbbac63e85375f5ecc965
nancylee713 <srlee713@gmail.com>
2019-11-03 16:11:01 -0700

    Add additional attribute for formatted_address in location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,8 +1,23 @@
 class Location
-  attr_reader :latitude, :longitude
+  attr_reader :latitude, :longitude, :address
 
   def initialize(data)
     @latitude = data[:results][0][:geometry][:location][:lat]
     @longitude = data[:results][0][:geometry][:location][:lng]
+    @address = parse_address(data)
+  end
+
+  def formatted_address
+    address.values.join(', ')
+  end
+
+  private
+
+  def parse_address(data)
+    {
+      city: data[:results][0][:address_components][0][:long_name],
+      state: data[:results][0][:address_components][2][:short_name],
+      country: data[:results][0][:address_components][3][:long_name]
+    }
   end
 end
