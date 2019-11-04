8d7c791878194886a2e2f3548025ecbf28d3927f
nancylee713 <srlee713@gmail.com>
2019-11-04 09:38:07 -0700

    Add an initial request spec test for antipode endpoint

---
+++ spec/requests/api/v1/antipode_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'Antipode API' do
+  before(:each) do
+    VCR.turn_off!
+    WebMock.allow_net_connect!
+  end
+
+  it 'sends forecast data' do
+    get '/api/v1/antipode?location=hongkong'
+
+    expect(response).to be_successful
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+  end
+end
