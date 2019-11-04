19565c56cf44a8ce8b63f9e71d71c0f5ed67eca8
nancylee713 <srlee713@gmail.com>
2019-11-04 10:40:43 -0700

    Update a request spec for antipode endpoint

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -6,11 +6,10 @@ class Api::V1::AntipodeController < ApplicationController
     location = Location.new(geocoding_data)
     lat = location.latitude
     lng = location.longitude
-
     # Find antipode lat and lng
     antipode_data = AntipodeService.get_data(lat: lat, lng: lng)
-    antipode_location = Location.new(antipode_data)
-
+    antipode_location = AntipodeLocation.new(antipode_data)
+    
     # Find current weather info for antipode
     weather_data = DarkSkyService.get_data(antipode_location)
     forecast_data = Forecast.new(antipode_location, weather_data)
--- spec/requests/api/v1/antipode_spec.rb
+++ spec/requests/api/v1/antipode_spec.rb
@@ -12,5 +12,9 @@ describe 'Antipode API' do
     expect(response).to be_successful
 
     parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed[:data][:type]).to eq("forecast")
+    expect(parsed[:data][:attributes][:location]).to eq("Jujuy")
+    expect(parsed[:data][:attributes].count).to eq(13)
   end
 end
