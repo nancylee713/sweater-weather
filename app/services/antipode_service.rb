adeb9b147fa8962a20f64c1ff9646d7ae071da04
nancylee713 <srlee713@gmail.com>
2019-11-04 10:39:48 -0700

    Modify faraday connection object

--- app/services/antipode_service.rb
+++ app/services/antipode_service.rb
@@ -4,19 +4,13 @@ class AntipodeService
   end
 
   def get_data(lat, lng)
-    response = conn.get do |req|
+    response = Faraday.get "http://amypode.herokuapp.com/api/v1/antipodes" do |req|
       req.params['lat'] = lat
-      req.params['lng'] = lng
+      req.params['long'] = lng
       req.headers['api_key'] = ENV['antipode_api_key']
     end
 
     json = JSON.parse(response.body, symbolize_names: true)
   end
 
-  def conn
-    Faraday.new(:url => "http://amypode.herokuapp.com/api/v1/antipodes/") do |faraday|
-      faraday.request :url_encoded
-      faraday.adapter Faraday.default_adapter
-    end
-  end
 end
