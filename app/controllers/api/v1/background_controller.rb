874ac660080550c9f786510df1dfa70a1eb83f49
nancylee713 <srlee713@gmail.com>
2019-11-04 14:33:10 -0700

    Add background image poro and serializer to show action

--- app/controllers/api/v1/background_controller.rb
+++ app/controllers/api/v1/background_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::BackgroundController < ApplicationController
   def show
     location = params[:location]
 
-    # Get data from Unsplash Api
-    image_data = UnsplashService.get_data(location)
+    raw_data = UnsplashService.get_data(location)
+    img_data = Image.new(raw_data)
+    render json: BackgroundImageSerializer.new(img_data)
   end
 end
