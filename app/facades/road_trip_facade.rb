89cbcef1cb49692544471d55f271e1a828cf885f
nancylee713 <srlee713@gmail.com>
2019-11-04 23:26:48 -0700

    Add private methods to RoadTripFacade

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -10,7 +10,23 @@ class RoadTripFacade
 
   private
 
+  attr_reader :origin, :destination
+
   def google_direction_service
     GoogleDirectionService.new(@origin, @destination)
+    GoogleDirectionService.new(origin, destination)
+  end
+
+  def google_direction_data
+    google_direction_service.get_direction
+  end
+
+  def time
+    google_direction_data[:duration]
+  end
+
+  def time_machine_service
+    TimeMachineService.new(google_direction_data, time)
+  end
   end
 end
