14f591c0c044ca2a0d8dd622b7f7c1a47b877883
nancylee713 <srlee713@gmail.com>
2019-11-05 19:17:30 -0700

    Modify response in forecast endpoint

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,16 +1,34 @@
 class ForecastSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id,
-            :location,
-            :current_time,
-            :current_sum,
-            :icon,
-            :current_temp,
-            :temp_high,
-            :temp_low,
-            :feels_like,
-            :humidity,
-            :visibility,
-            :uvIndex,
-            :summaries
+  attribute :id
+
+  attribute :overview do |data|
+    {
+      location: data.location,
+      current_time: data.current_time,
+      current_summary: data.current_sum,
+      icon: data.icon,
+      temp: data.current_temp,
+      temp_high: data.temp_high,
+      temp_low: data.temp_low
+    }
+  end
+
+  attribute :details do |data|
+    {
+      feels_like: data.feels_like,
+      humidity: data.humidity,
+      visibility: data.visibility,
+      uvIndex: data.uvIndex,
+      summaries: data.summaries
+    }
+  end
+
+  attribute :hourly_forecast do |data|
+    data.hourly_forecast.map! {|hour| hour.slice(:time, :temperature)}
+  end
+
+  attribute :daily_forecast do |data|
+    data.daily_forecast.map! {|day| day.slice(:time, :summary, :precipProbability, :precipType, :temperatureHigh, :temperatureLow)}
+  end
 end
